4e60d18f057272d2cdf745966775f22f5c9fffbc
Anonymized <any@example.com>
2016-04-20 22:33:54 +0200

    ignoring .js files and .map files

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,13 @@
+/**
+ * A sample component using the helloworld-app directive
+ * @author Johannes Konert
+ */
 import {Component} from 'angular2/core';
 
 @Component({
     selector: 'helloworld-app',
     template: '<h1>My First Angular 2 App</h1>'
 })
-export class AppComponent { }
+export class AppComponent {
+
+}
