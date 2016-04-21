cab2329617dcd5b25ec705077fd5a520b6c39942
Anonymized <any@example.com>
2016-04-21 07:32:21 +0200

    added bootstrapper main.ts

---
+++ app/main.ts
@@ -0,0 +1,8 @@
+/**
+ * A bootstrapper for the browser loading app.component
+ * @author Johannes Konert
+ */
+import {bootstrap}    from 'angular2/platform/browser';
+import {AppComponent} from './app.component';
+
+bootstrap(AppComponent);
