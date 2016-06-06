951d33490f975d318b82b167b0bc0e386b204d83
Anonymized <any@example.com>
2016-06-06 21:29:08 +0200

    delete mock heroes

--- app/mock-heroes.ts
+++
@@ -1,7 +0,0 @@
-import { Hero } from './hero';
-
-export var HEROES: Hero[] = [
-    new Hero(1,'Windstorm', 300, 'Olympia', 'Thunderbolt'), // Parameters defined in the Hero class (hero.ts-file)
-    new Hero(2,'Magneto', 35, 'Stalingrad', 'Neodym'),
-    new Hero(3,'Magma', 3000, 'Earth Core', 'Lava')
-];
