1e3acffc49cdb69e155837f2ff8deb005237216a
Anonymized <any@example.com>
2016-09-10 13:43:42 +0200

    Initial init, based on a clone of git@github.com:angular/quickstart.git changed config to work with WebStorm debugger and npm start likewise

--- systemjs.config.js
+++ systemjs.config.js
@@ -1,12 +1,8 @@
-/**
- * System configuration for Angular 2 samples
- * Adjust as necessary for your application needs.
- */
 (function (global) {
   System.config({
     paths: {
       // paths serve as alias
-      'npm:': 'node_modules/'
+      '@angular/*': 'node_modules/@angular/*'
     },
     // map tells the System loader where to look for things
     map: {
@@ -14,18 +10,18 @@
       app: 'app',
 
       // angular bundles
-      '@angular/core': 'npm:@angular/core/bundles/core.umd.js',
-      '@angular/common': 'npm:@angular/common/bundles/common.umd.js',
-      '@angular/compiler': 'npm:@angular/compiler/bundles/compiler.umd.js',
-      '@angular/platform-browser': 'npm:@angular/platform-browser/bundles/platform-browser.umd.js',
-      '@angular/platform-browser-dynamic': 'npm:@angular/platform-browser-dynamic/bundles/platform-browser-dynamic.umd.js',
-      '@angular/http': 'npm:@angular/http/bundles/http.umd.js',
-      '@angular/router': 'npm:@angular/router/bundles/router.umd.js',
-      '@angular/forms': 'npm:@angular/forms/bundles/forms.umd.js',
+      '@angular/core': '@angular/core/bundles/core.umd.js',
+      '@angular/common': '@angular/common/bundles/common.umd.js',
+      '@angular/compiler': '@angular/compiler/bundles/compiler.umd.js',
+      '@angular/platform-browser': '@angular/platform-browser/bundles/platform-browser.umd.js',
+      '@angular/platform-browser-dynamic': '@angular/platform-browser-dynamic/bundles/platform-browser-dynamic.umd.js',
+      '@angular/http': '@angular/http/bundles/http.umd.js',
+      '@angular/router': '@angular/router/bundles/router.umd.js',
+      '@angular/forms': '@angular/forms/bundles/forms.umd.js',
 
       // other libraries
-      'rxjs':                       'npm:rxjs',
-      'angular2-in-memory-web-api': 'npm:angular2-in-memory-web-api',
+      'rxjs':                       'node_modules/rxjs',
+      'angular2-in-memory-web-api': 'node_modules/angular2-in-memory-web-api',
     },
     // packages tells the System loader how to load when no filename and/or no extension
     packages: {
