8f707a7144c3235d9db2491358898c80aece5663
Anonymized <any@example.com>
2016-10-24 22:00:19 +0200

    Updated to AngularJS v2.1.1

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,14 @@
+/**
+ * a sample component using the my-app directive
+ * @author Johannes Konert
+ */
 import { Component } from '@angular/core';
 
 @Component({
     selector: 'my-app',
     template: '<h1>My First Angular App</h1>'
 })
-export class AppComponent { }
+export class AppComponent {
+
+
+}
