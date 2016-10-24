9930f2144d7998ba00f9628f1c19c66e28a7174d
Anonymized <any@example.com>
2016-10-24 22:18:04 +0200

    Added first variable binding {{name}}

--- app/app.component.ts
+++ app/app.component.ts
@@ -6,9 +6,12 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'my-app',
-    template: '<h1>My First Angular App</h1>'
+    template: '<h1>{{name}} First Angular App</h1>'
 })
 export class AppComponent {
+  name: string;
 
-
+  constructor() {
+    this.name = 'Our';
+  }
 }
