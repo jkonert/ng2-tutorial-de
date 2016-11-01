80e59fba9f6fb5b56b73369ec3be06bb1895216b
Anonymized <any@example.com>
2016-11-01 22:50:43 +0100

    Kommentare zur Erklärung von Angular Components

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,15 +2,21 @@
  * a sample component using the my-app directive
  * @author Johannes Konert
  */
+//Importiere Components
 import { Component } from '@angular/core';
 
+//Hallo Angular, die nun folgende Klasse ist ein Angular Component.
 @Component({
+	//Übrigens hat die Klasse auch noch diese Metadaten
     selector: 'my-app',
+    //Unser erster One-Way Bind
     template: '<h1>{{name}} First Angular App</h1>'
 })
 export class AppComponent {
+  //One Way Bind bezieht sich auf diese Variable
   name: string;
 
+  //Konstruktor initalisiert die Klasse
   constructor() {
     this.name = 'Our';
   }
