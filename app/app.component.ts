1adf98e5d22b6e239427180b8b03a2b8517dc9f7
Anonymized <any@example.com>
2016-11-02 21:03:20 +0100

    fixes for 02_Binding demo code

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,22 +1,24 @@
 /**
- * a sample component using the my-app directive
- * @author Johannes Konert
+ * A hero and its details
+ * @author Johannes Konert and contributing students
  */
-//Importiere Components
-import { Component } from '@angular/core';
 
-//Eine Klasse die kein Component ist
-export class Hero {
+import {Component, OnInit} from '@angular/core';
+
+/**
+ * Class to hold hero details. Will be soon in an own file
+ */
+class Hero {
   id: number;
   name: string;
+  nickname: string;
+  weapon: string;
+  noArms: boolean;
 }
 
-//Hallo Angular, die nun folgende Klasse ist ein Angular Component.
+
 @Component({
-	//Übrigens hat die Klasse auch noch diese Metadaten
     selector: 'my-app',
-    //One Way Bindings, Two Way Bindings und Event-Binding mit $event variable
-    // "`" kann man für multiline-strings benutzen.
     template: `
     		<h1>{{title}}</h1>
     		<h2>{{hero.name}} Details:</h2>
@@ -24,43 +26,59 @@ export class Hero {
     			<label>Id: </label> {{hero.id}}
     		</div>
     		<div>
-    			<label>Name: </label> <input [(ngModel)]="hero.name">
-    		</div>
-    		<button [disabled]=hero.noArms (click)="weaponChange()">change weapon!</button>
+    			<label>Name: </label> <input [(ngModel)]="hero.name" placeholder="name">
+    		</div>    		
     		<div>
     			<Label> Weapon: </Label> {{hero.weapon}}
     		</div>
+    		<button [disabled]="hero.noArms" (click)="weaponChange()">Toggle weapon!</button>
+    		<button (click)="hero.noArms=!hero.noArms">Toggle Hero armless</button>
+    		<p>Demo of keyUp</p>
     		<div>
-    			<input (keyup)="onKey($event)">
+    			<input (keyup)="onKeyUp($event)">
        			<p>{{values}}</p>
        		</div>
     `
 })
 export class AppComponent implements OnInit {
-  //One Way Bind bezieht sich auf diese Variable
 	title: string;
 	hero: Hero;
+  values: string = '';
 
-	//Konstruktor wird vor ngOnInit() aufgerufen
-	constructor() {
-	  this.title = "Tour Of Heroes"
+  constructor() {
+	  this.title = 'Tour Of Heroes';
 	  this.hero = {
 	  	id: 1,
-	  	name: "Windstorm",
-	  	nickname: "Wind"
-	  	weapon: "Klatsche",
-	  }
+	  	name: 'Windstorm',
+	  	nickname: 'Wind',
+	  	weapon: 'Sword',
+      noArms: true
+	  };
 	}
 
-	//Lifecycle Hook, wird nach dem ersten ngOnChanges() aufgerufen
-	ngOnInit() {
-  		this.hero.noArms = false	
+	/** demo on ngOnInit usage. Called by AngularJS after constructor */
+	ngOnInit(): void {
+  		this.hero.noArms = false;
 	}
 
+  /** called by template on button click. Willswitch between Sword and Axe */
 	weaponChange(): void {
-		this.hero.weapon = "Stinkefinger"
+		switch (this.hero.weapon) {
+      case 'Sword':
+        this.hero.weapon = 'Axe';
+        break;
+      default:
+      case 'Axe':
+        this.hero.weapon = 'Sword';
+        break;
+    }
 	}
-	onKey(event:any) {
+
+  /**
+   * function to be toggled on keyUp in template input. Will extend this.values by current input value
+   * @param event of input
+   */
+	onKeyUp(event: any) {
 		this.values += event.target.value + ' | ';
 	}
 }
