e578d992b9db424ef9fec2397e0419a5fc06468c
Anonymized <any@example.com>
2016-11-12 19:43:08 +0100

    examples of how to use *ngFor, *ngIf and [ngSwitch] inside app.component.ts

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,84 +1,170 @@
 /**
- * A hero and its details
- * @author Johannes Konert and contributing students
+ * a sample component using the my-app directive
+ * @author Johannes Konert
  */
-
 import {Component, OnInit} from '@angular/core';
+/**  * Class to hold hero details. Will be soon in an own file  */
 
-/**
- * Class to hold hero details. Will be soon in an own file
- */
-class Hero {
-  id: number;
-  name: string;
-  nickname: string;
-  weapon: string;
-  noArms: boolean;
+export class Hero {
+    id: number;
+    name: string;
+    nickname: string;
+    weapon: string;
+    noArms: boolean;
 }
 
+
+/**
+ * An array of Hero objects we want to display.
+ * @type {{id: number; name: string; nickname: string; weapon: string; noArms: boolean}[]}
+ */
+const HEROES: Hero [] = [
+    {id: 1, name: 'Windstorm', nickname: '', weapon: 'Sword', noArms: true },
+    {id: 2, name: 'Donald Trump', nickname: '', weapon: 'US-Air-Force', noArms: true},
+    {id: 3, name: 'Batman', nickname: '', weapon: 'Knife', noArms: true},
+    {id: 4, name: 'Thor', nickname: '', weapon: 'Hammer', noArms: true}
+];
 
 @Component({
     selector: 'my-app',
     template: `
-    		<h1>{{title}}</h1>
-    		<h2>{{hero.name}} Details:</h2>
-    		<div>
-    			<label>Id: </label> {{hero.id}}
-    		</div>
-    		<div>
-    			<label>Name: </label> <input [(ngModel)]="hero.name" placeholder="name">
-    		</div>    		
-    		<div>
-    			<Label> Weapon: </Label> {{hero.weapon}}
-    		</div>
-    		<button [disabled]="hero.noArms" (click)="weaponChange()">Toggle weapon!</button>
-    		<button (click)="hero.noArms=!hero.noArms">Toggle Hero armless</button>
-    		<p>Demo of keyUp</p>
-    		<div>
-    			<input (keyup)="onKeyUp($event)">
-       			<p>{{values}}</p>
-       		</div>
+                <h1>{{title}}</h1>
+                <ul class="heroes">
+                
+                <!-- for-loop to output each element of the component's "heroes" property as a <li>, to which a HERO[] has been
+                assigned by the ngOnInit() method. let i = index allows accessing the list's index.
+                The index starts at 1 and gets inserted automatically at the beginning of each <li> innerHTML -->
+                    <li *ngFor="let hero of heroes; let i = index" [class.selected]="hero === selectedHero" (click)="onSelect(hero)">
+                        <span class="badge">{{hero.id}}</span> {{hero.name}}                 
+                    </li>
+                </ul>
+                
+                <!-- the component's property "selectedHero" has the initial value "null" after the component was loaded.
+                As long as "selectedHero" is null, the section'S code is not getting inserted into the DOM.
+                By clicking onto one of the Heros, this Hero gets assigned to "selectedHero" and the section gets rendered
+                displaying the related Hero's value.-->
+                <div *ngIf="selectedHero">                
+                    <h2>{{selectedHero.name}} Details</h2>  
+                    <div>
+                        <label>Id: </label> {{selectedHero.id}} 
+                    </div>                
+                    <div>
+                        <label> Name: </label> <input ([ngModel])="selectedHero.name" placeholder="name">
+                    </div>      
+                    <div>
+                        <label> Weapon: </label> {{selectedHero.weapon}}
+                    </div>
+                    
+                    <button (click)="selectedHero.noArms =! selectedHero.noArms">Toogle Hero armless</button> 
+                                          
+                    <!-- Here are various ways shown of how to add a button and also other additional HTML code to a 
+                    component using [ngSwitch]. Notice that in each case the button refers to a different weaponChange
+                    method(), which is specified for a certain hero. -->                    
+                    <div [ngSwitch]="selectedHero.id">
+                        <div *ngSwitchCase="1">
+                            <button [disabled]="selectedHero.noArms" (click)="weaponChange1()">Toogle weapon!</button>
+                            <div> Random Div</div>
+                        </div>                       
+                        <div *ngSwitchCase="2">
+                            <button [disabled]="selectedHero.noArms" (click)="weaponChange2()">Toogle weapon!</button>
+                            <p>Random Paragaph</p>
+                            <p>Random Paragaph</p>
+                        </div>                        
+                        <div *ngSwitchCase="3">
+                            <button [disabled]="selectedHero.noArms" (click)="weaponChange3()">Toogle weapon!</button>
+                        </div>                      
+                        <button *ngSwitchCase="4" [disabled]="selectedHero.noArms" (click)="weaponChange4()">Toogle weapon!</button>
+                    </div>  
+              
+                </div>
+                <div>
+                    <p>Demo of keyUp</p>
+                    <input (keyup)="onKeyUp($event)">
+                    <p>{{values}}</p>
+                </div>
+
     `
 })
 export class AppComponent implements OnInit {
-	title: string;
-	hero: Hero;
-  values: string = '';
+    title: string;
+    heroes: Hero [];
+    values: string;
+    selectedHero: Hero;
 
-  constructor() {
-	  this.title = 'Tour Of Heroes';
-	  this.hero = {
-	  	id: 1,
-	  	name: 'Windstorm',
-	  	nickname: 'Wind',
-	  	weapon: 'Sword',
-      noArms: true
-	  };
-	}
 
-	/** demo on ngOnInit usage. Called by AngularJS after constructor */
-	ngOnInit(): void {
-  		this.hero.noArms = false;
-	}
-
-  /** called by template on button click. Willswitch between Sword and Axe */
-	weaponChange(): void {
-		switch (this.hero.weapon) {
-      case 'Sword':
-        this.hero.weapon = 'Axe';
-        break;
-      default:
-      case 'Axe':
-        this.hero.weapon = 'Sword';
-        break;
+    constructor() {
+        this.title = 'Tour Of Heroes';
+        this.heroes = null;
+        this.values = '';
+        this.selectedHero = null;
     }
-	}
 
-  /**
-   * function to be toggled on keyUp in template input. Will extend this.values by current input value
-   * @param event of input
-   */
-	onKeyUp(event: any) {
-		this.values += event.target.value + ' | ';
-	}
+    /** demo on ngOnInit usage. Called by AngularJS after constructor */
+    ngOnInit(): void {
+        this.heroes = HEROES;
+    }
+
+    /**  assigns an Hero to the Component's "selectedHero" property by clicking onto one of the Heros, which are listed on
+     * the *ngFor-generated hero list
+     * @param the Hero that has been clicked last */
+    onSelect(hero: Hero): void {
+        this.selectedHero = hero;
+    }
+
+    /** called by template on button click. Will switch between Sword and Axe for the hero with the id "1" */
+        weaponChange1(): void {
+        switch (this.selectedHero.weapon) {
+            case 'Sword':
+                this.selectedHero.weapon = 'Axe';
+                break;
+            case 'Axe':
+                this.selectedHero.weapon = 'Sword';
+                break;
+        }
+    }
+
+    /** called by template on button click. Will switch between US-Air-Force and US-Navy for the hero with the id "2" */
+    weaponChange2(): void {
+        switch (this.selectedHero.weapon) {
+            case 'US-Air-Force':
+                this.selectedHero.weapon = 'US-Navy';
+                break;
+            case 'US-Navy':
+                this.selectedHero.weapon = 'US-Air-Force';
+                break;
+        }
+    }
+
+    /** called by template on button click. Will switch between Knife and Pistole for the hero with the id "3" */
+    weaponChange3(): void {
+        switch (this.selectedHero.weapon) {
+            case 'Knife':
+                this.selectedHero.weapon = 'Pistole';
+                break;
+            case 'Pistole':
+                this.selectedHero.weapon = 'Knife';
+                break;
+        }
+    }
+
+    /** called by template on button click. Will switch between Hammer and Nail for the hero with the id "4" */
+    weaponChange4(): void {
+        switch (this.selectedHero.weapon) {
+            case 'Hammer':
+                this.selectedHero.weapon = 'Nail';
+                break;
+            case 'Nail':
+                this.selectedHero.weapon = 'Hammer';
+                break;
+        }
+    }
+
+    /** function to be toggled on keyUp in template input. Will extend this.values by current input value
+     * @param event of input
+    */
+    onKeyUp(event: any) {
+        this.values += event.target.value + ' | ';
+    }
+
+
 }
