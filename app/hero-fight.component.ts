aa371408606361a80485ce33fdab9c1bd7fe289d
Anonymized <any@example.com>
2016-11-28 16:23:44 +0100

    styling the fight a little bit

--- app/hero-fight.component.ts
+++ app/hero-fight.component.ts
@@ -3,7 +3,13 @@ import {Hero} from './hero';
 
 @Component({
   selector: 'hero-fight',
-  styles: [`.fight:active {
+  styles: [`section {
+                 border: #245899 solid;
+                 background-color: rgba(7,104,161,0.37);
+                 padding: 2em;
+            }
+            span {display: inline-block; margin: 0 0.5em;}
+            .fight:active {            
               background-color: red;
               transform: translateY(-8px);}
             .defence:active {
@@ -11,15 +17,15 @@ import {Hero} from './hero';
               transform: translateY(8px);}
 `],
   template: `
-                <div *ngIf="hero">      
-                   <div>
+                <section *ngIf="hero">      
+                   <div>Mode:
                     <input type="radio" name="mode" (click)="mode='fight'">fight
                     <input type="radio" name="mode" (click)="mode='defence'">defence                   
                   </div>
-                    <button [fightDirective] = "mode">{{hero.name}}</button>  
-                    <h1>VS</h1>
-                    <button [fightDirective] = "mode">{{villain}}</button>
-                </div>
+                  <button [fightDirective] = "mode">{{hero.name}}</button>  
+                  <span>VS</span>
+                  <button [fightDirective] = "mode">{{villain}}</button>
+                </section>
 `,
 })
 export class HeroFightComponent {
