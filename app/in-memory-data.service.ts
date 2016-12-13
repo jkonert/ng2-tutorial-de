029498e831986af364e5b73a12c6d4a8c4fbb2e1
Anonymized <any@example.com>
2016-12-13 16:45:35 +0100

    Usage of date pipe with parameters to showing the birthday of an hero

--- app/in-memory-data.service.ts
+++ app/in-memory-data.service.ts
@@ -3,16 +3,16 @@ import {InMemoryDbService} from "angular-in-memory-web-api";
 export class InMemoryDataService implements InMemoryDbService{
     createDb() {
         let heroes = [
-            {id: 11, name: 'Mr. Nice',  nickname: 'Nicy',   weapon: 'Smilebow', noArms: false},
-            {id: 12, name: 'Narco',     nickname: 'Nana',   weapon: '',         noArms: false},
-            {id: 13, name: 'Bombasto',  nickname: 'Bomber', weapon: 'Bomb',     noArms: false},
-            {id: 14, name: 'Celeritas', nickname: 'Celi',   weapon: 'Cello',    noArms: false},
-            {id: 15, name: 'Magneta',   nickname: 'Magi',   weapon: 'Electromagnet', noArms: false},
-            {id: 16, name: 'RubberMan', nickname: 'Rubs',   weapon: 'Shoe',     noArms: false},
-            {id: 17, name: 'Dynama',    nickname: 'Dyn',    weapon: 'Axe',      noArms: false},
-            {id: 18, name: 'Dr IQ',     nickname: 'Ici',    weapon: 'Riddle',   noArms: false},
-            {id: 19, name: 'Magma',     nickname: 'Red',    weapon: 'Fire',     noArms: true},
-            {id: 20, name: 'Tornado',   nickname: 'Squiddi', weapon: 'Wind',    noArms: true}
+            {id: 11, name: 'Mr. Nice',  nickname: 'Nicy',   weapon: 'Smilebow', noArms: false, birthday: new Date(1990, 3, 15)},
+            {id: 12, name: 'Narco',     nickname: 'Nana',   weapon: '',         noArms: false, birthday: new Date(1985, 3, 15)},
+            {id: 13, name: 'Bombasto',  nickname: 'Bomber', weapon: 'Bomb',     noArms: false, birthday: new Date(1982, 3, 15)},
+            {id: 14, name: 'Celeritas', nickname: 'Celi',   weapon: 'Cello',    noArms: false, birthday: new Date(1978, 3, 15)},
+            {id: 15, name: 'Magneta',   nickname: 'Magi',   weapon: 'Electromagnet', noArms: false, birthday: new Date(1999, 3, 15)},
+            {id: 16, name: 'RubberMan', nickname: 'Rubs',   weapon: 'Shoe',     noArms: false, birthday: new Date(1976, 3, 15)},
+            {id: 17, name: 'Dynama',    nickname: 'Dyn',    weapon: 'Axe',      noArms: false, birthday: new Date(1941, 3, 15)},
+            {id: 18, name: 'Dr IQ',     nickname: 'Ici',    weapon: 'Riddle',   noArms: false, birthday: new Date(1962, 3, 15)},
+            {id: 19, name: 'Magma',     nickname: 'Red',    weapon: 'Fire',     noArms: true, birthday: new Date(1982, 3, 15)},
+            {id: 20, name: 'Tornado',   nickname: 'Squiddi', weapon: 'Wind',    noArms: true, birthday: new Date(1991, 3, 15)}
         ];
         return {heroes};
     }
